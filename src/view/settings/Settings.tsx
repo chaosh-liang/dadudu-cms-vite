d996225b12a69a38e5e57e0439be2223bd2c9194
chaosh-liang <chao.liang.fly@gmail.com>
2021-10-05 00:01:07 +0800

    feat: 设置中心没功能，作为预留页面，添加插画 “敬请期待”

--- src/view/settings/Settings.tsx
+++ src/view/settings/Settings.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
+import styles from './Settings.module.scss'
+import stayTunedSvg from './images/staytuned.svg'
 
 const Settings: FC<RouteComponentProps> = (props) => {
-  return <div>Settings page</div>
+  return (
+    <div className={styles.container}>
+      <img className={styles['stay-tuned']} src={stayTunedSvg} />
+    </div>
+  )
 }
 
 export default Settings
